300efc1e0788de446a2edd856cf88d8ddc7b2c1d
degocab <degocab92@gmail.com>
2018-08-02 01:19:29 -0400

    npc spawner

--- Bounty-Hunt/Assets/Scripts/CharacterScript.cs
+++ Bounty-Hunt/Assets/Scripts/CharacterScript.cs
@@ -12,15 +12,17 @@ public class CharacterScript : MonoBehaviour {
 
 
      bool facingRight;
-
+    
     //public Animation anim;
     // Use this for initialization
     public Animator animator;
-    
+    Animation anim;
 	void Start () {
+        speed = Random.Range(7,10)/10f;
         npc = GameObject.FindGameObjectWithTag("NPC");
         animator = gameObject.GetComponent<Animator>();
         animator.speed = speed;
+        //anim["walk"].time = 0.5f;
         boolValue = (Random.Range(0, 2) == 0);
         if (!boolValue)
         {
@@ -38,13 +40,13 @@ public class CharacterScript : MonoBehaviour {
     {
         if (boolValue)
         {
-            transform.position += new Vector3((animator.speed * 2) * Time.deltaTime, 0, 0);
+            transform.position += new Vector3((animator.speed * 4) * Time.deltaTime, 0, 0);
         }
         else
         {
 
       
-            transform.position -= new Vector3((animator.speed * 2) * Time.deltaTime, 0, 0);
+            transform.position -= new Vector3((animator.speed *4) * Time.deltaTime, 0, 0);
         }
     }
 
---
+++ Bounty-Hunt/Assets/Scripts/NPCManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour {
+    public GameObject npc;
+    public float spawnTime = 10f;
+    public Transform[] spawnPoints;
+
+
+	// Use this for initialization
+	void Start () {
+        int i = 0;
+        int number = 25;
+        while (i < number)
+        {
+            InvokeRepeating("Spawn", spawnTime, 5);
+            //Invoke("Spawn",spawnTime);
+            i++;
+        }
+
+        //StartCoroutine(MyCounter(1));
+   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Spawn()
+    {
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        
+        Instantiate(npc,spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+    //IEnumerator MyCounter(int number)
+    //{
+    //    int i = 0;
+    //    while(i < number)
+    //    {
+    //        Invoke("Spawn", spawnTime);
+
+
+    //        yield return 0;
+    //    }
+    //}
+}
